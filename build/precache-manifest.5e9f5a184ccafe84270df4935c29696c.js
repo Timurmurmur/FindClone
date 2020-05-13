1534ee73102465bfd16cc7fc3ee34f464ce72ea1
Timurmurmur <bartimurmurmur@gmail.com>
2020-05-14 01:34:43 +0300

    BUILD

---
+++ build/precache-manifest.5e9f5a184ccafe84270df4935c29696c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bda6e54bcf1ee7a21ce5cbbfbfae2ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d020e1a4a46577315bee",
+    "url": "/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "f008b2e173d4c5e3b6f1",
+    "url": "/static/css/main.cc00b10c.chunk.css"
+  },
+  {
+    "revision": "d020e1a4a46577315bee",
+    "url": "/static/js/2.ede19423.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.ede19423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f008b2e173d4c5e3b6f1",
+    "url": "/static/js/main.9fdf3210.chunk.js"
+  },
+  {
+    "revision": "0782cb6010a6362aa175",
+    "url": "/static/js/runtime-main.41ad7e38.js"
+  }
+]);
